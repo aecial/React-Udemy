c24983be5320275a99e115ca2764c14944b8884b
Teddy Pascual <teddylayson2000@gmail.com>
2024-08-13 17:41:06 +0800

    add flexbox and styling

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hemwo Wormld!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter Your Goal" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of Goals....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    // borderColor: "red",
+    // borderWidth: 1,
+    // backgroundColor: "green",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 10,
   },
 });
