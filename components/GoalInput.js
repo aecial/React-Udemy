0cb70eaa560f8b019e3a06d9a59025baa0fe5850
Teddy Pascual <teddylayson2000@gmail.com>
2024-08-28 21:39:18 +0800

    add image to modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,11 @@
-import { StyleSheet, View, Button, TextInput, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  Modal,
+  Image,
+} from "react-native";
 function GoalInput({
   goalInputHandler,
   enteredGoalText,
@@ -9,9 +16,12 @@ function GoalInput({
   return (
     <Modal visible={openModal} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/lion.png")}
+        />
         <TextInput
           style={styles.textInput}
-          placeholder="Enter Your Goal"
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
@@ -31,14 +41,20 @@ const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
     justifyContent: "center",
-
+    backgroundColor: "#2C5C4F",
     gap: 10,
     padding: 8,
   },
+  image: {
+    width: 100,
+    height: 100,
+    marginHorizontal: "auto",
+  },
   textInput: {
-    borderColor: "gray",
+    borderColor: "white",
     borderWidth: 1,
     width: "100%",
+    color: "white",
     padding: 5,
   },
   buttonContainer: {
@@ -46,6 +62,7 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "center",
     gap: 16,
+    color: "white",
   },
 });
 
